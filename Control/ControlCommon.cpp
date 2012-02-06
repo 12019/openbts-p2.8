35391e78a3631d3129731591a2f6becf214f2b3d
Zabbal <suarev@stud.ntnu.no>
2012-02-06 18:32:05 +0400

    implement encryption activation logic

--- Control/ControlCommon.cpp
+++ Control/ControlCommon.cpp
@@ -160,16 +160,18 @@ unsigned Control::attemptAuth(GSM::L3MobileIdentity mobID, GSM::LogicalChannel* 
 	}
 	if(success) {// Ciphering Mode Procedures, GSM 04.08 3.4.7.
 	    if(gTMSITable.setKc(IMSI, Kc.c_str(), strtoul(cksn.c_str(), NULL, 10))) {
-		LOG(INFO) << "Ciphering Command Will Send";
+		LOG(INFO) << "Ciphering key set for LCH: " << LCH->setKc(Kc.c_str());
 		LCH->send(GSM::L3CipheringModeCommand());
-		LOG(INFO) << "Ciphering Command Sent";
+		LOG(INFO) << "Ciphering Mode Command sent, activating decryption.";
+		LCH->activateDecryption();
 		L3Frame* resp = LCH->recv();
 		LOG(INFO) << "Received";
 		if(!resp) { LOG(ERR) << "Ciphering Error"; }
-		else { LOG(INFO) << *resp << " Responce."; }
+		else { 
+		    LOG(INFO) << *resp << " Responce received, activating encryption."; 
+		    LCH->activateEncryption();
+		}
 		delete resp;
-		LCH->setKc(Kc.c_str());
-		LOG(INFO) << "Ciphering key set for LCH.";
 		return 0;
 	    }
 	    LOG(ERR) << "Failed to set Kc=" << Kc << " for IMSI=" << IMSI << " in TMSI table.";
