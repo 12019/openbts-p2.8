79ebee216cfcd3c5fb55fd825bf17325c6e0d22c
Zabbal <suarev@stud.ntnu.no>
2012-11-14 15:12:57 +0100

    Fix typos.

--- GSM/GSMLogicalChannel.h
+++ GSM/GSMLogicalChannel.h
@@ -229,12 +229,12 @@ public:
 
 	void activateEncryption(unsigned i = 1) { // use A5/1 by default
 	    if (mL1) mL1->activateEncryption(i);
-	    if (mSACCH) mL1->activateEncryption(i);
+	    if (mSACCH) ((LogicalChannel*)mSACCH)->activateEncryption(i);
 	}
 
 	void activateDecryption(unsigned i = 1) { // use A5/1 by default
 	    if (mL1) mL1->activateDecryption(i);
-	    if (mSACCH) mL1->activateEncryption(i);
+	    if (mSACCH) ((LogicalChannel*)mSACCH)->activateDecryption(i);
 	}
 
 	/** The TDMA parameters for the transmit side. */
